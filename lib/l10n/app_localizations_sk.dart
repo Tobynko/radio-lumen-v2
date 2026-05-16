58a674255dc89d9f9394159c7d1430ed75c1545c
Tobiáš Bulko <tobias.bulko@gmail.com>
2026-05-16 18:58:37 +0200

    chore: initialize localizations framework and configure Slovak arb keys

---
+++ lib/l10n/app_localizations_sk.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Slovak (`sk`).
+class AppLocalizationsSk extends AppLocalizations {
+  AppLocalizationsSk([String locale = 'sk']) : super(locale);
+
+  @override
+  String get appTitle => 'Rádio LUMEN';
+
+  @override
+  String get homeTitle => 'Domov';
+
+  @override
+  String get playerTitle => 'Prehrávač';
+
+  @override
+  String get scheduleTitle => 'Program';
+}
